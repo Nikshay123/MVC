c824a23bff15ed9c28b7dfe4ff6c917474cd4aa2
nikshay123 <nikshaykhandelwalk7891@gmail.com>
2025-02-06 16:58:43 +0530

    fefef

---
+++ Downloads/Week3Project/Week3Project/Week3Project/Models/IssuedBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week3Project.Models
+{
+    public class IssuedBook
+    {
+        public string Id { get; set; }
+        public string StudentId { get; set; }
+        public String BookId { get; set; }
+        public string IssueDate {  get; set; }
+        public string ReturnDate { get; set; }
+    }
+}
